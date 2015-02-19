54f786897bbc3bad69a61d3fd823b91dbea41177
Brandon Sneed <brandon@redf.net>
2015-02-19 07:25:13 -0800

    Added XCTestCase extensions to make up for lack of XCTAssertThrows in Swift.

--- THGFoundation/TestExtensions/XCTestCase+Exceptions.h
+++ THGFoundation/TestExtensions/XCTestCase+Exceptions.h
@@ -1,3 +1,17 @@
+/*
+ 
+ These files are not to be included in any targets except test case targets.
+ Simply drag them to your project and make sure only the Tests target is checked.
+ You'll be prompted to create an objc-bridging header.  Say yes, then add
+ this to the newly created header:
+ 
+ #import "XCTestCase+Exceptions.h"
+ 
+ These methods should now be accessible from within Swift without doing 
+ anything additional.
+ 
+ */
+
 @import Foundation;
 @import XCTest;
 
