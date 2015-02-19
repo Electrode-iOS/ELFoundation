4e263967e20547df1fd1474311607ab5ece35693
Brandon Sneed <brandon@redf.net>
2015-02-19 04:55:47 -0800

    Initial Commit

---
+++ THGFoundation/THGFoundation.h
@@ -0,0 +1,19 @@
+//
+//  THGFoundation.h
+//  THGFoundation
+//
+//  Created by Brandon Sneed on 2/19/15.
+//  Copyright (c) 2015 TheHolyGrail. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for THGFoundation.
+FOUNDATION_EXPORT double THGFoundationVersionNumber;
+
+//! Project version string for THGFoundation.
+FOUNDATION_EXPORT const unsigned char THGFoundationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <THGFoundation/PublicHeader.h>
+
+
