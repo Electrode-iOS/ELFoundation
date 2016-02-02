e1cd5cb2a7a33ce55b655ec1ba02065846faf6e8
Sam Grover <samgrover@gmail.com>
2016-02-02 15:17:34 -0800

    Added more tests.

--- ELFoundation/Utilities/Swizzling.swift
+++ ELFoundation/Utilities/Swizzling.swift
@@ -17,6 +17,15 @@ and a Shaman... and a few other engineers to figure out if there is a better way
 
 */
 
+/**
+Swizzles one method for another for a class. Subsequent to executing this, whenever the `original` method is called on 
+the `targetClass`, the runtime will execute `replacement` instead.
+
+- parameter targetClass: The class being targeted. For Swift class, this class must be a subclass of `NSObject`.
+- parameter original: The original method being swizzled. For Swift class, this method must be marked `dynamic`.
+- parameter replacement: The replacement method being swizzled. For Swift class, this method must be marked `dynamic`.
+*/
+
 public func unsafeSwizzle(targetClass: AnyObject.Type, original: Selector, replacement: Selector) {
     let originalMethod = class_getInstanceMethod(targetClass, original)
     let replacementMethod = class_getInstanceMethod(targetClass, replacement)
